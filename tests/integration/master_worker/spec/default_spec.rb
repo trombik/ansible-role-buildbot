95fa8a1e87bc67c92b5e9e3601f249717747fd3a
Tomoyuki Sakurai <y@trombik.org>
2019-06-06 17:04:35 +0900

    bugfix: overhaul integration test
    * add platformio build

--- tests/integration/master_worker/spec/default_spec.rb
+++ tests/integration/master_worker/spec/default_spec.rb
@@ -11,9 +11,30 @@ context "after provisioning finished" do
       result = current_server.ssh_exec("ping -c 1 #{server(:server1).server.address} && echo OK")
       expect(result).to match(/OK/)
     end
+
     it "fetch web interface top page" do
       r = current_server.ssh_exec("fetch -vv http://#{server(:server1).server.address}:8010")
       expect(r).to match(Regexp.escape("HTTP/1.1 200 OK"))
+    end
+
+    it "has pip-2.7 installed" do
+      r = current_server.ssh_exec("pip-2.7 --version")
+      expect(r).to match(/pip \d+\.\d+\.\d+\b/)
+    end
+
+    it "has platformio installed" do
+      r = current_server.ssh_exec("sudo -H -u buildbot pip-2.7 list")
+      expect(r).to match(/^platformio\b/)
+    end
+
+    it "has gcc48 installed" do
+      r = current_server.ssh_exec("pkg info gcc48")
+      expect(r).to match(/^gcc48-/)
+    end
+
+    it "has pio" do
+      r = current_server.ssh_exec("sudo -u buildbot /usr/local/buildbot_worker/.local/bin/pio --version")
+      expect(r).to match(/^PlatformIO/)
     end
   end
 
